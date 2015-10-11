0accc53d0d12a77c225b493d643a29ab42073a06
Vatazhka <vatazhka@mail.ru>
2015-10-12 01:18:45 +0200

    Change Google Analytics ID.

--- kobo.py
+++ kobo.py
@@ -39,7 +39,7 @@ def app(environ, start_response):
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-68702312-1', 'auto');
+  ga('create', 'UA-55369806-2', 'auto');
   ga('send', 'pageview');
 
 </script>
